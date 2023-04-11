0f6106908e50464b4041840eebba85c8291881dd
KadamJay <investinself123@gmail.com>
2023-04-11 21:45:41 +0530

    done

--- src/Arrays/LargestNumber.java
+++ src/Arrays/LargestNumber.java
@@ -4,31 +4,34 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+
 /**
  * @author jakadam on 2020-01-06
  */
 
 /**
- * Given a list of non negative integers, arrange them such that they form the largest number.
+ * Given a list of non negative integers, arrange them such that they form the
+ * largest number.
  * <p>
  * For example:
  * <p>
  * Given [3, 30, 34, 5, 9], the largest formed number is 9534330.
  * <p>
- * Note: The result may be very large, so you need to return a string instead of an integer.
+ * Note: The result may be very large, so you need to return a string instead of
+ * an integer.
  */
 public class LargestNumber {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>(Arrays.asList(3, 30, 34, 5, 9));
         System.out.println(largestNumber(list));
-        //expected : 9534330
+        // expected : 9534330
     }
 
     public static String largestNumber(final List<Integer> A) {
         /*
-        This problem can be solved by sorting strings, not sorting integer. Define a
-        comparator to compare strings by concat() right-to-left or left-to-right.
-        */
+         * This problem can be solved by sorting strings, not sorting integer. Define a
+         * comparator to compare strings by concat() right-to-left or left-to-right.
+         */
         int len = A.size();
         String[] str = new String[len];
 
@@ -37,14 +40,16 @@ public class LargestNumber {
             str[i] = String.valueOf(A.get(i));
         }
 
-        //write comparator to sort the String obj array in descending order of the strings
+        // write comparator to sort the String obj array in descending order of the
+        // strings
         Arrays.sort(str, new Comparator<String>() {
-            public int compare(String a, String b){
+            public int compare(String a, String b) {
                 return (b + a).compareTo(a + b);
             }
         });
 
-        // create string Builder from string array, we will perform some trimming operations
+        // create string Builder from string array, we will perform some trimming
+        // operations
         // on the output before returning
         StringBuilder sb = new StringBuilder();
         for (String numStr : str) {
@@ -52,7 +57,7 @@ public class LargestNumber {
         }
 
         // Remove the leading 0's
-        while (sb.charAt(0) == '0' && sb.length() > 1)
+        while (sb.length() > 1 && sb.charAt(0) == '0')
             sb.deleteCharAt(0);
 
         // convert stringBuilder to string and return
@@ -61,11 +66,13 @@ public class LargestNumber {
 }
 
 /**
- * Given a list of non negative integers, arrange them such that they form the largest number.
+ * Given a list of non negative integers, arrange them such that they form the
+ * largest number.
  * <p>
  * For example:
  * <p>
  * Given [3, 30, 34, 5, 9], the largest formed number is 9534330.
  * <p>
- * Note: The result may be very large, so you need to return a string instead of an integer.
+ * Note: The result may be very large, so you need to return a string instead of
+ * an integer.
  */
