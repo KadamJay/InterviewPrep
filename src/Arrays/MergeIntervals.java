bc6156c49740fc877c8596105c632e7c387e379c
KadamJay <investinself123@gmail.com>
2023-04-11 23:53:28 +0530

    done

--- src/Arrays/MergeIntervals.java
+++ src/Arrays/MergeIntervals.java
@@ -7,18 +7,22 @@ import java.util.ArrayList;
  */
 
 /**
- * Given a set of non-overlapping intervals, insert a new interval into the intervals
+ * Given a set of non-overlapping intervals, insert a new interval into the
+ * intervals
  * (merge if necessary).
  *
- * You may assume that the intervals were initially sorted according to their start times.
+ * You may assume that the intervals were initially sorted according to their
+ * start times.
  *
  * Example 1:
  *
- * Given intervals [1,3],[6,9] insert and merge [2,5] would result in [1,5],[6,9].
+ * Given intervals [1,3],[6,9] insert and merge [2,5] would result in
+ * [1,5],[6,9].
  *
  * Example 2:
  *
- * Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] would result in [1,2],[3,10],[12,16].
+ * Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] would result
+ * in [1,2],[3,10],[12,16].
  *
  * This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
  *
@@ -43,16 +47,20 @@ public class MergeIntervals {
 
     public ArrayList<Interval> insert(ArrayList<Interval> intervals, Interval newInt) {
         /**
-         * Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
-         You may assume that the intervals were initially sorted according to their start times.
-         Given intervals [1,3],[6,9] insert and merge [2,5] would result in [1,5],[6,9].
-
-         sol-
-         case1:(current.end < newIntr.start) -> res.add(current);
-         case2:(current.start > newIntr.end) -> res.add(newIntr); res.add(current);
-         case3:merge -> current and newIntr overlap
-         newIntr.start = Math.min(current.start, newIntr.start);
-         newIntr.end = Math.max(current.end, newIntr.end);
+         * Given a set of non-overlapping intervals, insert a new interval into the
+         * intervals (merge if necessary).
+         * You may assume that the intervals were initially sorted according to their
+         * start times.
+         * Given intervals [1,3],[6,9] insert and merge [2,5] would result in
+         * [1,5],[6,9].
+         * 
+         * solution -
+         * case1:(current.end < newIntr.start) -> res.add(current);
+         * case2:(current.start > newIntr.end) -> res.add(newIntr); res.add(current);
+         * case3:merge -> current and newIntr overlap
+         * newIntr.start = Math.min(current.start, newIntr.start);
+         * newIntr.end = Math.max(current.end, newIntr.end);
+         * 
          */
 
         ArrayList<Interval> ans = new ArrayList<>();
@@ -60,7 +68,7 @@ public class MergeIntervals {
         for (Interval cur : intervals) {
 
             if (newInt == null || cur.end < newInt.start) {
-                //newInt==null indicates, newInt has already been added
+                // newInt==null indicates, newInt has already been added
                 ans.add(cur);
             } else if (newInt.end < cur.start) {
                 ans.add(newInt);
@@ -82,22 +90,3 @@ public class MergeIntervals {
 /**
  * Tested on https://www.interviewbit.com/problems/merge-intervals/
  */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
