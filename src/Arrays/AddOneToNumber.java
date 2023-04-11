7ba37ce001a40531b650f4fc4288f9798758ed4b
KadamJay <investinself123@gmail.com>
2023-04-11 20:49:38 +0530

    update AddOneToNumber

--- src/Arrays/AddOneToNumber.java
+++ src/Arrays/AddOneToNumber.java
@@ -4,14 +4,13 @@ package Arrays;
  * @author jakadam on 2020-01-05
  */
 
-import java.util.ArrayList;
-
 /**
  * Given a non-negative number represented as an array of digits,
  *
  * add 1 to the number ( increment the number represented by the digits ).
  *
- * The digits are stored such that the most significant digit is at the head of the list.
+ * The digits are stored such that the most significant digit is at the head of
+ * the list.
  *
  * Example:
  *
@@ -21,50 +20,47 @@ import java.util.ArrayList;
  *
  * as 123 + 1 = 124.
  *
- *  NOTE: Certain things are intentionally left unclear in this question which you should
- *  practice asking the interviewer.
+ * NOTE: Certain things are intentionally left unclear in this question which
+ * you should
+ * practice asking the interviewer.
  * For example, for this problem, following are some good questions to ask :
- * Q : Can the input have 0’s before the most significant digit. Or in other words,
+ * Q : Can the input have 0’s before the most significant digit. Or in other
+ * words,
  * is 0 1 2 3 a valid input?
  * A : For the purpose of this question, YES
- * Q : Can the output have 0’s before the most significant digit? Or in other words,
+ * Q : Can the output have 0’s before the most significant digit? Or in other
+ * words,
  * is 0 1 2 4 a valid output?
- * A : For the purpose of this question, NO. Even if the input has zeroes before the most
+ * A : For the purpose of this question, NO. Even if the input has zeroes before
+ * the most
  * significant digit.
  */
 
-//first push from mac air
-import java.util.*;
 public class AddOneToNumber {
 
-    public static void main(String []args){
+    public int[] plusOne(int[] digits) {
+        int len = digits.length;
 
-        int[] arr={2, 3, 7, 11, 15};
-        ArrayList<Integer> a = new ArrayList<>();
-        for(int i=0;i <arr.length;i++){
-            a.add(arr[i]);
-        }
-        int b=9;
-
-
-        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
-        // int index=0;
-        for(int i=0; i< a.size();i++){
-            if(map.get(a.get(i)) != null){
-
-            }
-            else{
-                map.put(a.get(i),i);
-            }
-
-        }
-        // int in=0;
-        for(int i=0; i< a.size();i++){
-            if(map.containsKey(b-a.get(i))){
-                System.out.print(i);
-                System.out.print(map.get(b-a.get(i)));
+        for (int i = len - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            } else {
+                // digits[i] has to be 9 in this condition, set the numbers to 0 till they are 9
+                digits[i] = 0;
             }
         }
+
+        // if control reaches here, means array was of the for 999 and return should be
+        // 10000
+        // copy contents of input array and prepend 1
+        int[] ans = new int[len + 1];
+        ans[0] = 1;
+        for (int i = 1; i < ans.length - 1; i++) {
+            ans[i] = digits[i];
+        }
+
+        return ans;
     }
 
 }
