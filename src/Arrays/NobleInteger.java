a0eb6c34869c37f5000d5455a82015025143a1cd
KadamJay <investinself123@gmail.com>
2023-04-11 22:25:31 +0530

    done

--- src/Arrays/NobleInteger.java
+++ src/Arrays/NobleInteger.java
@@ -9,42 +9,68 @@ import java.util.Collections;
  */
 
 /**
- * Given an integer array, find if an integer p exists in the array such that the number of
+ * Given an integer array, find if an integer p exists in the array such that
+ * the number of
  * integers greater than p in the array equals to p
  * If such an integer is found return 1 else return -1.
+ * 
+ * Input 1:
+ * 
+ * A = [3, 2, 1, 3]
+ * Asorted = [1, 2, 3, 3]
+ * Output 1:
+ * 
+ * 1
+ * 
+ * Input 2:
+ * 
+ * A = [1, 1, 3, 3]
+ * 
+ * Output 2:
+ * 
+ * -1
  */
 public class NobleInteger {
     public static void main(String[] args) {
-        ArrayList<Integer> A= new ArrayList<>(Arrays.asList(7, 3, 16, 10));
+        int[] A = new int[] { 7, 3, 16, 10 };
         System.out.println(solve(A));
 
-        A= new ArrayList<>(Arrays.asList(-1, -9, -2, -78, 0));
+        A = new int[] { -1, -9, -2, -78, 0 };
         System.out.println(solve(A));
     }
 
-    public static int solve(ArrayList<Integer> A) {
-        /*
-        1. Sort the Array arr[] in ascending order. This step takes (O(nlogn)).
-        2. Iterate through the array. Compare the value of index i to the number of elements
-        after index i. If arr[i] equals the number of elements after arr[i], it is a noble
-        Integer. Condition to check: (A[i] == length-i-1). This step takes O(n).
-        Note: Array may have duplicate elements. So, we should skip the elements
-        (adjacent elements in the sorted array) that are same.
-        */
-        int len=A.size();
-        Collections.sort(A);
+    /*
+     * 1. Sort the Array arr[] in ascending order. This step takes (O(nlogn)).
+     * 2. Iterate through the array. Compare the value of index i to the number of
+     * elements
+     * after index i. If arr[i] equals the number of elements after arr[i], it is a
+     * noble
+     * Integer. Condition to check: (A[i] == length-i-1). This step takes O(n).
+     * Note: Array may have duplicate elements. So, we should skip the elements
+     * (adjacent elements in the sorted array) that are same.
+     */
+    public static int solve(int[] A) {
+        int len = A.length;
+        Arrays.sort(A);
 
-        for(int i=0; i<len; i++){
-            if(A.get(i)==(len-1)-i)
+        for (int i = 0; i < len; i++) {
+
+            // skip, in case adjacenet elements are same,
+            // this is because we need to find number of elements, smaller than the cur ele
+            if (i < A.length - 1 && A[i] == A[i + 1])
+                continue;
+
+            // `(len - 1) - i` represents the number of elements smaller than A[i], in this
+            // case.
+            if (A[i] == (len - 1) - i)
                 return 1;
         }
         return -1;
     }
+
 }
 
-
 /*
-http://www.geeksforgeeks.org/noble-integers-in-an-array-count-of-greater-elements-is-equal-to-value/
+ * http://www.geeksforgeeks.org/noble-integers-in-an-array-count-of-greater-
+ * elements-is-equal-to-value/
  */
-
-
