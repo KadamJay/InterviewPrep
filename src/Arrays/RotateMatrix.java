514c463aef4ab79b42278b3509e641dccdad2424
KadamJay <investinself123@gmail.com>
2023-04-12 23:16:00 +0530

    done

--- src/Arrays/RotateMatrix.java
+++ src/Arrays/RotateMatrix.java
@@ -92,3 +92,31 @@ public class RotateMatrix {
         // System.out.println(Arrays.toString(A.toArray()));
     }
 }
+
+// with arrays
+
+class RotateMatrixArray {
+    public void rotate(int[][] matrix) {
+
+        int n = matrix.length;
+
+        // transpose matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // swap the columns
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][(n - 1) - j];
+                matrix[i][(n - 1) - j] = temp;
+            }
+        }
+    }
+}
