adc52cf95a5a1f3fdef68edc0eda541161e72a5f
KadamJay <investinself123@gmail.com>
2023-04-17 08:55:25 +0530

    done

--- src/Arrays/RepeatedMissingNum.java
+++ src/Arrays/RepeatedMissingNum.java
@@ -7,6 +7,7 @@ import java.util.List;
 There are certain problems which are asked in the interview to also check how you take care of overflows
 in your problem.
 This is one of those problems.
+
 Please take extra care to make sure that you are type-casting your ints to long properly and at all places.
 Try to verify if your solution works if number of elements is as large as 105
 
@@ -37,63 +38,63 @@ A = 3, B = 4
 */
 public class RepeatedMissingNum {
     public static void main(String args[]) {
-        List<Integer> nums= Arrays.asList(3,1,2,5,3);
-        final RepeatedMissingNumSolver solver= new RepeatedMissingNumSolver();
+        List<Integer> nums = Arrays.asList(3, 1, 2, 5, 3);
+        final RepeatedMissingNumSolver solver = new RepeatedMissingNumSolver();
         solver.solve(nums);
-        //System.out.println(solver.solve(nums)) ;
+        // System.out.println(solver.solve(nums)) ;
     }
 
 }
 
-class RepeatedMissingNumSolver{
-    public void solve(final List<Integer> nums){
-        long n =nums.size();
+class RepeatedMissingNumSolver {
+    public void solve(final List<Integer> nums) {
+        long n = nums.size();
 
-        //(A+A+C+D) -> Given seq sum
-        long givenSum=0;
-        for(int num:nums)
-            givenSum+=(long)num;
+        // (A+A+C+D) -> Given seq sum
+        long givenSum = 0;
+        for (int num : nums)
+            givenSum += (long) num;
 
-        //(A+B+C+D) -> Actual seq sum
-        long actualSum= (long)(n*(n+1)/2);
+        // (A+B+C+D) -> Actual seq sum
+        long actualSum = (long) (n * (n + 1) / 2);
 
-        //(A-B)
-        long aSubB=givenSum-actualSum;
+        // (A-B)
+        long aSubB = givenSum - actualSum;
 
-        //(A2+A2+C2+D2)-> Given seq sum
-        long givenSumSq=0;
-        for(int num:nums)
-            givenSumSq+=(long)num*(long)num;
+        // (A2+A2+C2+D2)-> Given seq sum
+        long givenSumSq = 0;
+        for (int num : nums)
+            givenSumSq += (long) num * (long) num;
 
-        //(A2+B2+C2+D2)-> Actual seq sum
-        long actualSumSq= (long)(n*(n+1)*(2*n+1)/6);
+        // (A2+B2+C2+D2)-> Actual seq sum
+        long actualSumSq = (long) (n * (n + 1) * (2 * n + 1) / 6);
 
-        //A2-B2 =>(A+B)(A-B)
-        long a2SubB2=givenSumSq-actualSumSq;
+        // A2-B2 =>(A+B)(A-B)
+        long a2SubB2 = givenSumSq - actualSumSq;
 
-        //(A+B)=((A+B)(A-B)/(A-B))
-        long aPlusB=a2SubB2/aSubB;
+        // (A+B)=((A+B)(A-B)/(A-B))
+        long aPlusB = a2SubB2 / aSubB;
 
-        //A=((A+B)+(A-B))/2
-        int a=(int)((aPlusB+aSubB)/2);
+        // A=((A+B)+(A-B))/2
+        int a = (int) ((aPlusB + aSubB) / 2);
         // B=((A+B)-(A-B))/2
-        int b=(int)((aPlusB-aSubB)/2);
-        System.out.println("a="+a);
-        System.out.println("b="+b);
+        int b = (int) ((aPlusB - aSubB) / 2);
+        System.out.println("a=" + a);
+        System.out.println("b=" + b);
     }
 }
 /*
-        (A+A+C+D) -> Given seq sum
-       -(A+B+C+D) -> Actual seq sum
-       =(A-B)
-
-        (A2+A2+C2+D2)-> Given seq sum
-       -(A2+B2+C2+D2)-> Actual seq sum
-       =A2-B2 =>(A+B)(A-B)
-
-        (A+B)=((A+B)(A-B)/(A-B))
-
-        Now, we have (A+B) and (A-B)
-        A=((A+B)+(A-B))/2
-        B=((A+B)-(A-B))/2
+ * (A+A+C+D) -> Given seq sum
+ * -(A+B+C+D) -> Actual seq sum
+ * =(A-B)
+ * 
+ * (A2+A2+C2+D2)-> Given seq sum
+ * -(A2+B2+C2+D2)-> Actual seq sum
+ * =A2-B2 =>(A+B)(A-B)
+ * 
+ * (A+B)=((A+B)(A-B)/(A-B))
+ * 
+ * Now, we have (A+B) and (A-B)
+ * A=((A+B)+(A-B))/2
+ * B=((A+B)-(A-B))/2
  */
