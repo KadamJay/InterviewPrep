b0795c05ca153c43c030b00682864cb81e8fc0db
KadamJay <investinself123@gmail.com>
2023-06-23 11:27:22 +0530

    Essential150 init

--- src/Arrays/PascalsTriangle.java
+++ src/Arrays/PascalsTriangle.java
@@ -2,33 +2,50 @@ package Arrays;
 
 import java.util.ArrayList;
 
+/*
+ *  Input Format: N = 5
+
+    Result:
+        1
+    1 1
+    1 2 1
+    1 3 3 1
+    1 4 6 4 1
+
+    Explanation: There are 5 rows in the output matrix. Each row corresponds to each one of the rows in the image shown above.
+
+    Input Format: N = 1
+
+    Result: 
+        1
+ */
 public class PascalsTriangle {
-    public static void main(String args[]){
-        int numRows=5;
-        ArrayList<ArrayList<Integer>> res ;
+    public static void main(String args[]) {
+        int numRows = 5;
+        ArrayList<ArrayList<Integer>> res;
         PascalsTriangleSolver solver = new PascalsTriangleSolver();
-        res=solver.solve(numRows);
-        //print the pascal triangle
-        for(int i=0; i<numRows; i++){
-            for(int j=0; j<=i; j++){
-                System.out.print(res.get(i).get(j)+" ");
+        res = solver.solve(numRows);
+        // print the pascal trianglex
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(res.get(i).get(j) + " ");
             }
             System.out.println("\n");
         }
     }
 }
 
-class PascalsTriangleSolver{
-    public ArrayList<ArrayList<Integer>> solve(int numRows){
-        ArrayList<ArrayList<Integer>> res =new ArrayList<>();
+class PascalsTriangleSolver {
+    public ArrayList<ArrayList<Integer>> solve(int numRows) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
 
-        for(int i=0; i<numRows; i++){
+        for (int i = 0; i < numRows; i++) {
             ArrayList<Integer> inner = new ArrayList<>();
-            for(int j=0; j<=i; j++){
-                if(j==0||j==i){
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
                     inner.add(1);
-                }else{
-                    inner.add(res.get(i-1).get(j-1)+res.get(i-1).get(j));
+                } else {
+                    inner.add(res.get(i - 1).get(j - 1) + res.get(i - 1).get(j));
                 }
             }
             res.add(inner);
---
+++ src/Essential150/Arrays/ContainsDuplicate.java
@@ -0,0 +1,71 @@
+package Essential150.Arrays;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+ Problem    :   https://leetcode.com/problems/contains-duplicate/
+ Solutions  :   https://youtu.be/3OamzN90kPg
+ Approaches :
+ 
+    | No. | Approach                             | Time Complexity | Space Complexity |
+    |-----|--------------------------------------|-----------------|------------------|
+    |  1  | Sorting                              |   O(n log n)    |       O(1)       |
+    |-----|--------------------------------------|-----------------|------------------|
+    |  2  | Compare each element with every other|      O(n^2)     |       O(1)       |
+    |     | element using nested loops           |                 |                  |
+    |-----|--------------------------------------|-----------------|------------------|
+    |  3  | Set                                  |       O(n)      |       O(n)       |
+
+
+ */
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> hash = new HashSet<>();
+        for (int num : nums) {
+            if (hash.contains(num)) {
+                return true;
+            }
+            hash.add(num);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ContainsDuplicateTest test = new ContainsDuplicateTest();
+        test.testContainsDuplicate();
+        test.testNoDuplicates();
+        test.testEmptyArray();
+        test.testSingleElement();
+    }
+}
+
+class ContainsDuplicateTest {
+    public void testContainsDuplicate() {
+        int[] nums = { 1, 2, 3, 1 };
+        ContainsDuplicate containsDuplicate = new ContainsDuplicate();
+        boolean result = containsDuplicate.containsDuplicate(nums);
+        assert result == true : "Failed testContainsDuplicate";
+    }
+
+    public void testNoDuplicates() {
+        int[] nums = { 1, 2, 3, 4 };
+        ContainsDuplicate containsDuplicate = new ContainsDuplicate();
+        boolean result = containsDuplicate.containsDuplicate(nums);
+        assert result == false : "Failed testNoDuplicates";
+    }
+
+    public void testEmptyArray() {
+        int[] nums = {};
+        ContainsDuplicate containsDuplicate = new ContainsDuplicate();
+        boolean result = containsDuplicate.containsDuplicate(nums);
+        assert result == false : "Failed testEmptyArray";
+    }
+
+    public void testSingleElement() {
+        int[] nums = { 1 };
+        ContainsDuplicate containsDuplicate = new ContainsDuplicate();
+        boolean result = containsDuplicate.containsDuplicate(nums);
+        assert result == false : "Failed testSingleElement";
+    }
+}
